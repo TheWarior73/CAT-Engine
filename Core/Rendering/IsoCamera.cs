5e22eb3eb22dc771517f3e3d144840131a895a42
Jemko <jemko23@t-online.de>
2026-05-26 00:33:38 +0200

    feat: Add IsoCamera Base

---
+++ Core/Rendering/IsoCamera.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Rendering
+{
+    public class IsoCamera
+    {
+        public IsoCamera() 
+        {
+            position = new Vector2(0, 0);
+        }
+
+        public IsoCamera(Vector2 startPosition)
+        {
+            position = startPosition;
+        }
+
+        public Vector2 position;
+        public float zoom;
+
+        public void SetPosition(Vector2 inPosition)
+        {
+            position = inPosition;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return position;
+        }
+
+        public void SetZoom(float inZoom)
+        {
+            zoom = inZoom;
+        }
+
+        public float GetZoom()
+        {
+            return zoom;
+        }
+    }
+}
