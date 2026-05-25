b7dbd0588e3874aeb5e016faee9468c417fe28e5
Jemko <jemko23@t-online.de>
2026-05-26 00:46:15 +0200

    feat: Add Basic Scene/Scene Management classes

---
+++ Core/SceneBase/IsoSceneManager.cs
@@ -0,0 +1,38 @@
+﻿using CAT_Engine.Core.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.SceneBase
+{
+    public class IsoSceneManager
+    {
+        public IsoSceneManager() { }
+
+        public IsoScene currentScene = null;
+        public IsoCamera activeCamera = null;
+
+        public T LoadScene<T>() where T : IsoScene
+        {
+            if(currentScene != null)
+            {
+                currentScene.Unload();
+                currentScene = null;
+            }
+
+            T sceneInstance = (T)(Activator.CreateInstance(typeof(T)));
+            sceneInstance.Load();
+
+            currentScene = sceneInstance;
+
+            return sceneInstance;
+        }
+
+        public void Init()
+        {
+            
+        }
+    }
+}
