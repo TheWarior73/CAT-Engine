0e8c10ed7245bb4295ae6bd94ce4bb576ff19c0c
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-05-26 01:13:30 +0200

    feat: IsoRenderInterface

---
+++ Core/Rendering/Interfaces/IsoRenderInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Rendering.Interfaces
+{
+    internal interface IsoRenderInterface
+    {
+
+        public void Render();
+
+    }
+}
