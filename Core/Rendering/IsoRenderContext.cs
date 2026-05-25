055d094b11f37d68aa85cf2baba82eb48841eb9b
Jemko <jemko23@t-online.de>
2026-05-26 01:09:22 +0200

    feat: Add Render Context and Renderer

---
+++ Core/Rendering/IsoRenderContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Rendering
+{
+    public class IsoRenderContext
+    {
+        public IsoCamera? camera;
+        public Dictionary<string, object> extras { get; } = new();
+
+        public T GetExtra<T>(string key)
+        {
+            extras.TryGetValue(key, out object? value);
+            return (T)value;
+        }
+    }
+
+    public class IsoRenderContextBuilder
+    {
+        IsoRenderContext renderContext;
+
+        public IsoRenderContextBuilder()
+        {
+            renderContext = new IsoRenderContext();
+        }
+
+        public IsoRenderContextBuilder SetCamera(IsoCamera camera)
+        {
+            renderContext.camera = camera;
+            return this;
+        }
+
+        public IsoRenderContextBuilder AddExtra(string key, object? value)
+        {
+            renderContext.extras[key] = value;
+            return this;
+        }
+
+        public IsoRenderContext Build()
+        {
+            return renderContext;
+        }
+    }
+}
---
+++ Core/Rendering/IsoRenderer.cs
@@ -0,0 +1,28 @@
+﻿using CAT_Engine.Core.SceneBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Rendering
+{
+    public class IsoRenderer
+    {
+        public IsoRenderer() { }
+        public IsoRenderContext RenderContext { get; set; }
+        public void RenderScene(IsoScene scene) 
+        {
+            SetupRenderContext();
+            
+        }
+
+        public void SetupRenderContext()
+        {
+            IsoRenderContextBuilder builder = new IsoRenderContextBuilder();
+            builder.SetCamera(IsoSceneManager.activeCamera);
+
+            RenderContext = builder.Build();
+        }
+    }
+}
