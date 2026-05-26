951e5312131db50f15260c88bbf9341c1c09afdd
Jemko <jemko23@t-online.de>
2026-05-27 00:25:31 +0200

    feat: IsoRenderableTileObject

---
+++ Core/Tiles/TileObjects/IsoRenderableTileObject.cs
@@ -0,0 +1,23 @@
+﻿using CAT_Engine.Core.Tiles.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Tiles.TileObjects
+{
+    public class IsoRenderableTileObject : IsoTileObject, IsoRenderableTileComponentInterface
+    {
+        public IsoRenderableTileObject() { }
+
+        private string currentTextureId = "base";
+
+        public string GetTextureID()
+        {
+            return currentTextureId;
+        }
+
+        public void SetTextureID(string textureID) => currentTextureId = textureID;
+    }
+}
