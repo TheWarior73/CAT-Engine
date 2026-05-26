8296177081ed5dd645b7255b08d724ec010ad912
Jemko <jemko23@t-online.de>
2026-05-26 02:17:09 +0200

    chore: Seperate IsoOpenableTileComponent from IsoTileComponent.cs

--- Core/Tiles/TileComponents/IsoTileComponent.cs
+++ Core/Tiles/TileComponents/IsoTileComponent.cs
@@ -7,13 +7,4 @@ using System.Threading.Tasks;
 namespace CAT_Engine.Core.Tiles.TileComponents
 {
     public abstract class IsoTileComponent { }
-
-    public class IsoOpenableTileComponent : IsoTileComponent
-    {
-        public bool isOpen;
-        public string openedTexture;
-        public string closedTexture;
-
-        string GetTexture() => isOpen ? openedTexture : closedTexture;
-    }
 }
