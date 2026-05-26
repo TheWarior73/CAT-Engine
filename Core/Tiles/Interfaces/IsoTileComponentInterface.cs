9557a2214de84192df965230a3940e4d2c1e1541
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-05-26 21:51:41 +0200

    feat: Tile Component Interface

---
+++ Core/Tiles/Interfaces/IsoTileComponentInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Tiles.Interfaces
+{
+    /// <summary>
+    /// Interface that include anything that every TileComponent needs
+    /// </summary>
+    internal interface IsoTileComponentInterface
+    {
+        /// <summary>
+        /// Should retrieve the texture of the current tile component
+        /// </summary>
+        /// <returns>The current tile component's texture</returns>
+        public string GetTexture();
+    }
+}
--- Core/Tiles/TileComponents/IsoOpenableTileComponent.cs
+++ Core/Tiles/TileComponents/IsoOpenableTileComponent.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CAT_Engine.Core.Tiles.Interfaces;
 
 namespace CAT_Engine.Core.Tiles.TileComponents
 {
-    public class IsoOpenableTileComponent : IsoTileComponent
+    public class IsoOpenableTileComponent : IsoTileComponent, IsoTileComponentInterface
     {
         public bool isOpen;
         public string openedTexture;
         public string closedTexture;
 
+        // Interface implementation
         public string GetTexture() => isOpen ? openedTexture : closedTexture;
     }
 }
