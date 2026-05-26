03552326f9b07fd0580833b6c92c9b70bdc0a442
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-05-26 23:31:32 +0200

    feat: add/remove
    [  ] Object to Square (TODO)
    [x] Square to ZStack
    [x] ZStack to Chunk

--- Core/Tiles/IsoTileDataStructures.cs
+++ Core/Tiles/IsoTileDataStructures.cs
@@ -29,17 +29,80 @@ namespace CAT_Engine.Core.Tiles
     {
         public IsoTileSquare() {}
         public List<IsoTileObject> objects = new List<IsoTileObject>();
+
+        // TODO: Add/Remove an object from the square
     }
 
+    /// <summary>
+    /// Represents an Isometric Chunk Layer
+    /// </summary>
     public class IsoTileZStack
     {
         public IsoTileZStack() {}
 
         //96x96 = 9.216 TOTAL Tiles in a Stack
-        public static byte ZSTACK_WIDTH = 96;
-        public static byte ZSTACK_HEIGHT = 96;
+        public static byte ZSTACK_MAX_X = 96;
+        public static byte ZSTACK_MAX_Y= 96;
 
-        public IsoTileSquare[,] tiles = new IsoTileSquare[ZSTACK_WIDTH, ZSTACK_HEIGHT];
+        public IsoTileSquare[,] tiles = new IsoTileSquare[ZSTACK_MAX_X, ZSTACK_MAX_Y];
+
+        /// <summary>
+        /// Converts the global coordinates of a square to a local chunk coordinate
+        /// </summary>
+        /// <param name="globalPos">The global position of the square</param>
+        /// <returns></returns>
+        public static IntVector2 CalculateSquareCoordinatesInZstack(IntVector2 globalPos)
+        {
+            IntVector2 stackPos = new();
+            stackPos.x = globalPos.x % ZSTACK_MAX_X;
+            stackPos.y = globalPos.y % ZSTACK_MAX_Y;
+
+            return stackPos;
+        }
+
+        /// <summary>
+        /// Creates and adds a new <see cref="IsoTileSquare"/> to a <see cref="IsoTileZStack"/>
+        /// </summary>
+        /// <param name="globalPos">The global position of the square to be created</param>
+        /// <returns>The newly created square</returns>
+        /// <exception cref="Exception"></exception>
+        public IsoTileSquare CreateTileSquare(IntVector2 globalPos)
+        {
+            IsoTileSquare newTileSquare = null;
+
+            IntVector2 chunkPos = CalculateSquareCoordinatesInZstack(globalPos);
+
+            // If there is no square on the coordinates, we add it.
+            if (tiles[chunkPos.x, chunkPos.y] == null) {
+                newTileSquare = new IsoTileSquare();
+                tiles[chunkPos.x, chunkPos.y] = newTileSquare;
+            } else // There is already a square !
+            {
+                throw new Exception("CreateTileSquare: A TileSquare already exists at (global)Pos (" + globalPos.x + ", " + globalPos.y + ")");
+            }
+
+            return newTileSquare;
+        }
+
+        /// <summary>
+        /// Removes a <see cref="IsoTileSquare"/> from a <see cref="IsoTileZStack"/>
+        /// </summary>
+        /// <param name="globalPos">The global position of the square to be removed</param>
+        public void RemoveTileSquare(IntVector2 globalPos)
+        {
+            IntVector2 chunkPos = CalculateSquareCoordinatesInZstack(globalPos);
+
+            tiles[chunkPos.x, chunkPos.y] = null;
+        }
+
+        /// <summary>
+        /// Clears an IsoTileZstack
+        /// </summary>
+        public void ClearStack()
+        {
+            tiles = new IsoTileSquare[ZSTACK_MAX_X, ZSTACK_MAX_Y];
+        }
+
     }
 
     /// <summary>
@@ -59,5 +122,42 @@ namespace CAT_Engine.Core.Tiles
         // Dict of stacks in the chunk
         // Int key = Z Height
         public Dictionary<int, IsoTileZStack> stacks;
+
+        /// <summary>
+        /// Calculates the chunk coordinates in the world relative to a global position
+        /// </summary>
+        /// <param name="globalPos"></param>
+        /// <returns></returns>
+        public static IntVector2 CalculateChunkCoords(IntVector2 globalPos)
+        {
+            IntVector2 chunkPos = new();
+
+            chunkPos.x = (int)Math.Floor((float)globalPos.x / IsoTileZStack.ZSTACK_MAX_X);
+            chunkPos.y = (int)Math.Floor((float)globalPos.y / IsoTileZStack.ZSTACK_MAX_Y);
+
+            return chunkPos;
+        }
+
+        public IsoTileZStack CreateZStack(int zIndex)
+        {
+            IsoTileZStack newZStack = null;
+            
+            // If stack is free at this index, we create a new stack and add it
+            if (!stacks?.ContainsKey(zIndex) ?? false)
+            {
+                newZStack = new();
+                stacks[zIndex] = newZStack;
+            } else // If stack is not free, we throw an Exception
+            {
+                throw new Exception("CreateZStack: zStack already has a member at index " + zIndex);
+            }
+
+            return newZStack;
+        }
+
+        public void RemoveZStack(int zIndex)
+        {
+            stacks?.Remove(zIndex); // Will throw an exception is the index does not exist in the stack
+        }
     }
 }
