c17c87d6e7bc1fda964cdae7b6d17b081e81d8dc
Jemko <jemko23@t-online.de>
2026-05-28 03:28:04 +0200

    feat: Test game content

---
+++ Games/CATSandboxGame/Scenes/GameScene.cs
@@ -0,0 +1,17 @@
+﻿using CAT_Engine.Core.SceneBase;
+using CAT_Engine.Core.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CATSandboxGame.Scenes
+{
+    public class GameScene : IsoScene
+    {
+        public GameScene() { }
+
+        public IsoTilemap tilemap = new IsoTilemap();
+    }
+}
