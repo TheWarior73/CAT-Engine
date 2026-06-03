76ef8593a5b733b655eedc9ac32be01c11804d3d
Jemko <jemko23@t-online.de>
2026-06-04 00:28:41 +0200

    feat: Input Manager

---
+++ Core/Input/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using CAT_Engine.Core.Debug;
+using CAT_Engine.Core.Interfaces;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Input
+{
+    public class InputManager : IsoUpdateableInterface
+    {
+        public void Initialize()
+        {
+        }
+
+        public void Update(float delta)
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Z))
+            {
+                IsoLogger.Assert(false, "I have been pressed yay!");
+            }
+        }
+    }
+}
