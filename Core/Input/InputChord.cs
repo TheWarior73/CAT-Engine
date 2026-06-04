d3530539f236dbaaebde4988fb35a7deb589129c
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-06-04 02:30:45 +0200

    feat: InputChord

---
+++ Core/Input/InputChord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+
+namespace CAT_Engine.Core.Input
+{
+
+    [Flags]
+    public enum Modifiers
+    {
+        None = 0,
+        Shift = 1 << 0,
+        Alt = 1 << 1,
+        Control = 1 << 2,
+    }
+
+    public struct InputChord
+    {
+        private Keys Key { get; set; }
+        private byte modifiers;
+
+        public InputChord()
+        {
+            Key = Keys.None;
+            modifiers = (byte) Modifiers.None;
+        }
+
+        #region Modifiers
+        public readonly bool GetModifier(Modifiers mod)
+        {
+            return (modifiers & (byte) mod) != 0;
+        }
+
+        public void SetModifier(Modifiers mod, bool value) {
+            if (value)
+            {
+                modifiers |= (byte)mod;
+            }
+            else
+            {
+                modifiers &= (byte)~mod;
+            }
+        }
+        #endregion
+    }
+}
