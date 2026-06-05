4abc222ebd9ecdd5854f1c7a21cbd509f6b7604a
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-06-05 14:11:23 +0200

    chore: moved Axis struct to it's own file

---
+++ Core/Input/Axis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAT_Engine.Core.Input
+{
+    public struct Axis
+    {
+        public float scale { get; set; }
+        public InputChord[] keybinds;
+
+        public Axis()
+        {
+            scale = 0.0f;
+            keybinds = null;
+        }
+    }
+}
