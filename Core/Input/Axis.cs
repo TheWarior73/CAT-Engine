2c16e7ce565a334e120854cf9a055e4940ae3c72
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-06-20 17:42:32 +0200

    feat: Input section documentation

--- Core/Input/Axis.cs
+++ Core/Input/Axis.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace CAT_Engine.Core.Input
 {
+    /// <summary>
+    /// Represents an axis with it's associated scale [-1.0, 1.0] and <see cref="InputChord"/> (as keybind associated to the Axis trigger)
+    /// </summary>
     public struct Axis
     {
+        /// <summary>
+        /// The scale of the axis
+        /// </summary>
         public float scale { get; set; }
+
+        /// <summary>
+        /// The keybinds associated with the axis
+        /// </summary>
         public InputChord[] keybinds;
 
+        /// <summary>
+        /// The default axis constructor<br/>
+        /// - scale = 0.0f<br/>
+        /// - keybinds = null
+        /// </summary>
         public Axis()
         {
             scale = 0.0f;
